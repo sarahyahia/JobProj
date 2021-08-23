2d89d1ceea48b115a02af83af08c3534eb0cafcb
Sarah <sarah.ali2810@gmail.com>
2021-08-23 03:07:48 +0200

    NavBar

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,32 @@
+import './_navbar.scss';
+
+import React from 'react';
+import {Nav, Container, Navbar} from 'react-bootstrap';
+import { NavLink, Link} from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <>
+           <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Job Club</Navbar.Brand>
+                <Nav className="me-auto">
+                <Link className="navLink" to={`/`} style={{ textDecoration: 'none' }}> 
+                Home
+              </Link> 
+
+              <Link  className="navLink"  to={`/Shows`} style={{ textDecoration: 'none'}}> 
+                Add a Job
+              </Link>
+
+              <Link className="navLink" to={`/emp/register`} style={{ textDecoration: 'none' }}> 
+                Search for Jobs
+            </Link>
+                </Nav>
+            </Container>
+          </Navbar> 
+        </>
+    )
+}
+
+export default NavBar
--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header/Header'
-import "./_homeScreen.scss"
+import "./_homeScreen.scss";
+import NavBar from '../../components/navbar/NavBar';
 
 const HomeScreen = () => {
     return (
         <>
+            <NavBar/>
             <Header/>
         </>
     )
